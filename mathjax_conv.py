3845649c113bd15cbf5301ece02850e32bb47fa2
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-02-02 17:27:19 +0330

    Add mathjax_conv.py

---
+++ mathjax_conv.py
@@ -0,0 +1,8 @@
+import io, sys
+
+for fname in sys.argv[1:]:
+    with io.open(fname) as f:
+        result = f.read()
+    result = result.replace('\\\\\\\\', '\\\\').replace('\\\\(', '$').replace('\\\\)', '$').replace('\\\\[', '$').replace('\\\\]', '$')
+    with io.open(fname, 'w') as f:
+        f.write(result)
