b3abc10bfbfcd9c9c44de15c903c23cbe423db87
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-02-14 01:53:29 +0330

    Enable syntax highlighting in pdf

--- mathjax_conv.py
+++ mathjax_conv.py
@@ -3,7 +3,14 @@ import io, sys
 for fname in sys.argv[1:]:
     with io.open(fname) as f:
         result = f.read()
-    result = result.replace('\\\\\\\\', '\\\\').replace('\\\\(', '$').replace('\\\\)', '$').replace('\\\\[', '$').replace('\\\\]', '$')
-    with io.open(fname, 'w') as f:
+    result = (
+        result.replace("\\\\\\\\", "\\\\")
+        .replace("\\\\(", "$")
+        .replace("\\\\)", "$")
+        .replace("\\\\[", "$")
+        .replace("\\\\]", "$")
+        .replace('```python=', '```python')
+    )
+    with io.open(fname, "w") as f:
         f.write("\n\pagebreak\n")
         f.write(result)
